f1b780497918a11384bc422fe9a431a8f297d97d
Simone Summo <simone.summo@immobiliare.it>
2026-07-05 15:36:55 +0200

    chore: add navigation compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.navigation.compose)
 
     // Main - debug only
     debugImplementation(libs.androidx.ui.tooling)
