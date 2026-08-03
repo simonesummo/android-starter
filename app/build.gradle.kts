eb79379191474d291d32131243115458e4205caf
Simone Summo <simone.summo@immobiliare.it>
2026-08-03 08:50:19 +0200

    chore: update R8 configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,20 +16,20 @@ android {
         versionCode = 1
         versionName = "1.0"
 
+        multiDexEnabled = true
+
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         debug {
             optimization {
-                enable = true
+                enable = false
             }
         }
         release {
-            isMinifyEnabled = true
-            isShrinkResources = true
             optimization {
-                enable = false
+                enable = true
             }
         }
     }
